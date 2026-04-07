612620bc89b7da94823c737e74e4213552c6c17c
RayssaAraujo12 <rayaraujo809@gmail.com>
2026-04-07 19:48:35 -0300

    Update Livro.java

--- dart_web_application_1/Modelos/Livro.java
+++ dart_web_application_1/Modelos/Livro.java
@@ -1,3 +1,14 @@
-public class Livro {
+class Livro {
+  int id;
+  String titulo;
+  String autor;
+  int ano;
 
+  // Construtor
+  Livro(this.id, this.titulo, this.autor, this.ano);
+
+  // Método para exibir detalhes
+  void exibirDetalhes() {
+    print('ID: $id | Título: $titulo | Autor: $autor | Ano: $ano');
+  }
 }
